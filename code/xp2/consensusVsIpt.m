6a898b59e8e9260a13b73b07b27b534478b828e0
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2019-10-07 17:56:48 +0200

    done renorm figs

--- code/xp2/consensusVsIpt.m
+++ code/xp2/consensusVsIpt.m
@@ -1,6 +1,6 @@
 
-% load ticelJudgments
-% load clusters
+%load ticelJudgments
+%load clusters
 
 ins = unique(instruments);
 mod = unique(modes);
@@ -11,12 +11,14 @@ for k=1:length(instruments)
 end
 
 mi = zeros(length(ins));
+min = zeros(length(ins));
 for k=1:length(ins)
     for l=1:length(ins)
         for m=1:length(in)
             for n=1:length(in)
                 if in(m) == k && l == in(n) && ensemble(m) == ensemble(n)
-                    mi(k, l) = mi(k, l)+1./sum(in==in(m));
+                    mi(k, l) = mi(k, l)+1;
+                    min(k, l) = min(k, l)+1./(sum(in==in(m))+sum(in==in(n)));
                 end
             end
         end
@@ -27,23 +29,26 @@ end
 
 figure(1)
 colormap bone
-[~, ~, perm] = dendrogram(linkage(mi, 'ward'));
-imagesc(1-mi(perm, perm))
+[~, ~, perm] = dendrogram(linkage(min, 'ward'));
+imagesc(1-min(perm, perm))
 set(gca, 'xtick', 1:length(ins))
 set(gca, 'xticklabels', ins(perm))
 set(gca, 'ytick', 1:length(ins))
 set(gca, 'yticklabels', ins(perm))
 set(gca, 'fontsize', 16)
+xtickangle(45)
 
 saveas(gcf, 'consensusVsI.png')
 
 moo = zeros(length(mod));
+moon = zeros(length(mod));
 for k=1:length(mod)
     for l=1:length(mod)
         for m=1:length(mo)
             for n=1:length(mo)
                 if mo(m) == k && l == mo(n) && ensemble(m) == ensemble(n)
-                    moo(k, l) = moo(k, l)+1./sum(mo==mo(m));
+                    moo(k, l) = moo(k, l)+1;
+                    moon(k, l) = moon(k, l)+1./(sum(mo==mo(m))+sum(mo==mo(n)));
                 end
             end
         end
@@ -51,13 +56,13 @@ for k=1:length(mod)
 end
 figure(2)
 colormap bone
-[~, ~, perm] = dendrogram(linkage(moo, 'ward'));
-imagesc(1-moo(perm, perm))
+[~, ~, perm] = dendrogram(linkage(moon, 'ward'));
+imagesc(1-moon(perm, perm))
 set(gca, 'xtick', 1:length(mod))
 set(gca, 'xticklabels', mod(perm))
 set(gca, 'ytick', 1:length(mod))
 set(gca, 'yticklabels', mod(perm))
-% xtickangle(45)
+xtickangle(45)
 set(gca, 'fontsize', 16)
 saveas(gcf, 'consensusVsPt.png')
 return

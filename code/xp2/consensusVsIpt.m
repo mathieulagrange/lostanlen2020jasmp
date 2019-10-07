afcef92652b39cd362d9fed709623f9c92fda8ff
Mathieu Lagrange <mathieu.lagrange@cnrs.fr>
2019-10-07 17:36:13 +0200

    norm figs

--- code/xp2/consensusVsIpt.m
+++ code/xp2/consensusVsIpt.m
@@ -1,6 +1,6 @@
 
-%load ticelJudgments
-%load clusters
+% load ticelJudgments
+% load clusters
 
 ins = unique(instruments);
 mod = unique(modes);
@@ -16,14 +16,18 @@ for k=1:length(ins)
         for m=1:length(in)
             for n=1:length(in)
                 if in(m) == k && l == in(n) && ensemble(m) == ensemble(n)
-                    mi(k, l) = mi(k, l)+1;
+                    mi(k, l) = mi(k, l)+1./sum(in==in(m));
                 end
             end
         end
     end
 end
+
+
+
 figure(1)
-[~, ~, perm] = dendrogram(linkage(mi, 'ward'))
+colormap bone
+[~, ~, perm] = dendrogram(linkage(mi, 'ward'));
 imagesc(1-mi(perm, perm))
 set(gca, 'xtick', 1:length(ins))
 set(gca, 'xticklabels', ins(perm))
@@ -39,20 +43,21 @@ for k=1:length(mod)
         for m=1:length(mo)
             for n=1:length(mo)
                 if mo(m) == k && l == mo(n) && ensemble(m) == ensemble(n)
-                    moo(k, l) = moo(k, l)+1;
+                    moo(k, l) = moo(k, l)+1./sum(mo==mo(m));
                 end
             end
         end
     end
 end
 figure(2)
-[~, ~, perm] = dendrogram(linkage(moo, 'ward'))
+colormap bone
+[~, ~, perm] = dendrogram(linkage(moo, 'ward'));
 imagesc(1-moo(perm, perm))
 set(gca, 'xtick', 1:length(mod))
 set(gca, 'xticklabels', mod(perm))
 set(gca, 'ytick', 1:length(mod))
 set(gca, 'yticklabels', mod(perm))
-xtickangle(45)
+% xtickangle(45)
 set(gca, 'fontsize', 16)
 saveas(gcf, 'consensusVsPt.png')
 return
